b0eac934f1058fca6728a7af9433a4b8f8c73999
Toby <toby@tobymeehan.com>
2020-05-08 17:17:32 +0100

    Add where builder
    Added builder for SQL where clauses, using a linq expression

---
+++ TobyMeehan.Sql/QueryBuilder/IWhereBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace TobyMeehan.Sql.QueryBuilder
+{
+    public interface IWhereBuilder
+    {
+        SqlClause ToSql<T>(Expression<Predicate<T>> expression);
+    }
+}
---
+++ TobyMeehan.Sql/QueryBuilder/SqlClause.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace TobyMeehan.Sql.QueryBuilder
+{
+    public class SqlClause
+    {
+        public string Sql { get; set; }
+        public Dictionary<string, object> Parameters { get; set; }
+
+        public static SqlClause FromSql(string sql)
+        {
+            return new SqlClause
+            {
+                Sql = sql
+            };
+        }
+
+        public static SqlClause FromParameter(int count, object value)
+        {
+            return new SqlClause
+            {
+                Parameters = { { count.ToString(), value } },
+                Sql = $"@{count}"
+            };
+        }
+
+        public static SqlClause FromCollection(ref int countStart, IEnumerable<object> values)
+        {
+            var parameters = new Dictionary<string, object>();
+            var sql = new StringBuilder("(");
+
+            foreach (var value in values)
+            {
+                parameters.Add(countStart.ToString(), value);
+                sql.Append($"@{countStart}");
+                countStart++;
+            }
+
+            if (sql.Length == 1)
+            {
+                sql.Append("null,");
+            }
+
+            sql[sql.Length - 1] = ')';
+
+            return new SqlClause
+            {
+                Parameters = parameters,
+                Sql = sql.ToString()
+            };
+        }
+
+        public static SqlClause Concat(string @operator, SqlClause operand)
+        {
+            return new SqlClause
+            {
+                Parameters = operand.Parameters,
+                Sql = $"({@operator} {operand.Sql}"
+            };
+        }
+
+        public static SqlClause Concat(SqlClause left, string @operator, SqlClause right)
+        {
+            return new SqlClause
+            {
+                Parameters = left.Parameters.Union(right.Parameters).ToDictionary(x => x.Key, x => x.Value),
+                Sql = $"({left.Sql} {@operator} {right.Sql})"
+            };
+        }
+    }
+}
