9b176a7ea51a323e0bbec6ff96282550c15499ef
Toby <toby@tobymeehan.com>
2020-05-09 15:03:34 +0100

    Add SqlString
    Added class representing sql literal, which should not be parameterised

---
+++ TobyMeehan.Sql/SqlString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TobyMeehan.Sql
+{
+    public class SqlString
+    {
+        public SqlString(string value)
+        {
+            _value = value;
+        }
+
+        private string _value;
+
+        public static implicit operator string(SqlString s) => s._value;
+        public static implicit operator SqlString(string s) => new SqlString(s);
+
+        public override string ToString() => _value;
+    }
+}
